4109134bf1354694df9648bd8093de9cf8ad10f0
Thomas Hodges <thodges@javascriptonline.org>
2023-10-17 15:43:26 -0700

    remove comments

--- src/interactivity/d3Graphs/HandKTableGraph/parabolaValuesArray.js
+++ src/interactivity/d3Graphs/HandKTableGraph/parabolaValuesArray.js
@@ -4,7 +4,6 @@ const parabolaValuesArray = (min = -3, max = 3, freq = 1) => {
   for (let k = min; k <= max; k += freq) {
     allValues.push([k, k ** 2]);
   }
-  // if (allValues[allValues.length - 1][0] < max) allValues.push([max, max ** 2]);
   return allValues;
 };
 
--- src/interactivity/d3Graphs/MaclaurenChart/cosineValues.js
+++ src/interactivity/d3Graphs/MaclaurenChart/cosineValues.js
@@ -4,8 +4,6 @@ const cosValuesArray = (min = -2, max = 4, freq = 0.125) => {
   for (let k = min; k <= max; k += freq) {
     allValues.push([Math.PI * k, Math.cos(Math.PI * k)]);
   }
-  // if (allValues[allValues.length - 1][0] < max)
-  //   allValues.push([max, Math.cos(max)]);
   return allValues;
 };
 
